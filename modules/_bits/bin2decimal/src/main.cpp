b401d75112b93bc7d65b8886d41d4406753b1e87
Oleg Kolomiytsev <kolomiytsev.oleg@gmail.com>
2018-12-11 00:02:43 +0200

    Added test project to convert binary nubmer to decimal one.

---
+++ modules/_bits/bin2decimal/src/main.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+
+int main(int argc, char* argv[])
+{
+	int num = 110011;
+	// 1*1 + 1*2 + 0*4 + 0*8 + 1*16 + 1*32 ...
+	std::cout << "binary number: " << num << std::endl;
+
+	int decimal = 0;
+	int degree = 1;
+	while (num != 0)
+	{
+		int remainder = num % 10;
+		num /= 10;
+		std::cout << remainder << "*" << degree << std::endl;
+		remainder *= degree;
+		degree *= 2;
+		decimal += remainder;
+	}
+
+	std::cout << "decimal number: " << decimal << std::endl;
+	return 0;
+
+	return 0;
+}
