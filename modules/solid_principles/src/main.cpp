785cdcd0e3ba547c4ec326b39b9b879ac98c6a5e
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-06-10 23:04:46 +0300

    srp

--- modules/solid_principles/src/main.cpp
+++ modules/solid_principles/src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+/*
+S - single responsibility
+    "Every software component should have one and only one responsibility"
+    "Every software component should have one and only one reason to change"
+O
+L
+I
+D
+*/
+
 int main(int argc, char *argv[]) {
     std::cout << "111111111" << std::endl;
     return 0;
