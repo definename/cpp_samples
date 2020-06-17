d9ffe7ac9d4e1b1068d2c05ae9bb1eba4f7d8c96
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-06-18 00:10:58 +0300

    never call virtual from construct and destructor

--- modules/_virtual/virtual_method/src/main.cpp
+++ modules/_virtual/virtual_method/src/main.cpp
@@ -1,5 +1,31 @@
-#include <cstdlib>
+#include <iostream>
+
+class A {
+	public:
+	A() {
+		run();
+	}
+	~A() {
+		run();
+	}
+	virtual void run() const {
+		std::cout << "A" << std::endl;
+	}
+};
+
+class B: public A {
+	public:
+	B() = default;
+	~B() = default;
+
+	void run() const override {
+		std::cout << "WILL NEVER BE CALLED" << std::endl;
+	}
+};
 
 int main(int argc, char* argv[]) {
+	B d;
+	B *dd = new B();
+	delete dd;
 	return EXIT_SUCCESS;
 }
