5eb86fc533c2bd58932208139b041991ac04569a
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-06-18 23:30:31 +0300

    Condition, function

--- modules/_functional/functional/src/pch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//////////////////////////////////////////////////////////////////////////
-// STL includes and definitions.
-//
-
-#include <iostream>
-#include <string>
-#include <functional>
--- modules/_thread/condition_var/src/main.cpp
+++ modules/_thread/condition_var/src/main.cpp
@@ -1,4 +1,11 @@
-#include "pch.h"
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <stack>
+#include <future>
+#include <memory>
+#include <thread>
+#include <vector>
 
 std::mutex mutex_;
 std::condition_variable cv_;
@@ -7,49 +14,47 @@ bool done_ = false;
 void Waiter();
 void Cook();
 
-int main(int argc, char* argv[])
-{
-	try
-	{
-		auto t1 = std::async(std::launch::async, Waiter);
-		auto t2 = std::async(std::launch::async, Cook);
+int main(int argc, char* argv[]) {
+	try {
+		std::vector<std::thread> v;
+		v.push_back(std::thread(Waiter));
+		v.push_back(std::thread(Cook));
+
+		for (auto& t: v) {
+			t.join();
+		}
+
+		// auto t1 = std::async(std::launch::async, Waiter);
+		// auto t2 = std::async(std::launch::async, Cook);
 	}
-	catch (const std::exception& e)
-	{
+	catch (const std::exception& e) {
 		std::cerr << "Error occurred: " << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
 }
 
-void Waiter()
-{
-	while (true)
-	{
+void Waiter() {
+	while (true) {
+		std::unique_lock l(mutex_);
+		cv_.wait(l, []() { return done_; });
+
 		{
-			std::unique_lock l(mutex_);
-			cv_.wait(l, []() { return done_; });
+			l.unlock();
+			std::this_thread::sleep_for(std::chrono::seconds(6));
+			l.lock();
 
-			{
-				l.unlock();
-				std::this_thread::sleep_for(std::chrono::seconds(6));
-				l.lock();
-
-				std::cout << "Has been eaten!!" << std::endl;
-				done_ = false;
-			}
+			std::cout << "Has been eaten!!" << std::endl;
+			done_ = false;
 		}
 	}
 }
 
-void Cook()
-{	
-	while (true)
-	{
+void Cook() {
+	while (true) {
 		{
 			std::unique_lock l(mutex_);
-			if (!cv_.wait_for(l, std::chrono::seconds(1), []() { return !done_; }))
-			{
+			if (!cv_.wait_for(l, std::chrono::seconds(1), []() { return !done_; })) {
 				std::cout << "Is not eaten!!" << std::endl;
 				continue;
 			}
