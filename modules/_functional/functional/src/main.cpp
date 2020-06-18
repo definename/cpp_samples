f0894fe51f6c06007e3d6a9f2382289d7d5782e9
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-06-18 12:12:56 +0300

    std function with lambda

--- modules/_functional/functional/src/main.cpp
+++ modules/_functional/functional/src/main.cpp
@@ -1,31 +1,28 @@
-#include "pch.h"
+#include <iostream>
+#include <string>
+#include <functional>
 
-using Functor = std::function<void(const std::string& data)>;
+using Functor = std::function<void(const std::string&)>;
 
-void Func(const std::string& data);
+void FuncToCall(const std::string& data) {
+    std::cout << data << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
-    Functor func_(std::bind(&Func, std::placeholders::_1));
+    Functor func_(std::bind(&FuncToCall, std::placeholders::_1));
+    Functor func1_ = func_;
 
-    Functor func1_, func2_, func3_;
-    func1_ = func_;
-    func2_ = func_;
-    func3_ = func_;
-
-    if (func_)
+    if (func_) {
         func_("0");
-    if (func1_)
+    }
+    if (func1_) {
         func1_("1");
-    if (func2_)
-        func2_("2");
-    if (func3_)
-        func3_("3");
+    }
 
-    return 0;
-}
+    auto l = [](const std::string&d){ std::cout << d << " called" << std::endl;};
+    Functor func_lambda(l);
+    func_lambda("lambda");
 
-void Func(const std::string& data)
-{
-    std::cout << data << std::endl;
+    return EXIT_SUCCESS;
 }
