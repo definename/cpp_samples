8936ab5c95922f833a87efe05ef5f6d8dd2c396e
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-06-25 22:13:31 +0300

    utils were removed

--- modules/_mydatastructure/myforwardlist/src/main.cpp
+++ modules/_mydatastructure/myforwardlist/src/main.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
-
 #include "myforwardlist.h"
-#include "myforwarlistutil.h"
 
 #include <forward_list>
 
 int main(int argc, char *argv[]) {
-    int arr[] = { 1, 22, 33, 44, 55, 66, 77, 88, 99, 100 };
-
-    // dsutil::FwdNode *head;
-    // dsutil::fwd_list_create(&head, arr, 10);
-
-    // dsutil::fwd_list_display(head);
-    // dsutil::fwd_list_display_recursive(head);
-    // std::cout << "list size:" << dsutil::fwd_list_count(head) << std::endl;
-    // std::cout << "max is:" << dsutil::fwd_list_find_max(head) << std::endl;
-
-    // dsutil::FwdNode* found = dsutil::fwd_list_search(head, 77);
-    // std::cout << (found ? "found" : "not found") << std::endl;
-
     {
         std::forward_list<int> l = {1, 2, 2, 97, 99};
 
