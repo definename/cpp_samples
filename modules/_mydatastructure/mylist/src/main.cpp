2025b38cd6817002c7fdde5d61939822a19e5d42
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2020-07-02 20:37:31 +0300

    doubly linked list template skeleton,push_back

--- modules/_mydatastructure/mylist/src/main.cpp
+++ modules/_mydatastructure/mylist/src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "mylist.h"
+#include <list>
+
+using MyListType = int;
+using MyList = dslist::List<MyListType>;
 
 int main(int argc, char *argv[]) {
+    MyList l;
+    l.push_back(1);
+    std::cout << l.size() << std::endl;
+
     return EXIT_SUCCESS;
 }
--- modules/_mydatastructure/mylist/src/mylist.h
+++ modules/_mydatastructure/mylist/src/mylist.h
@@ -5,8 +5,50 @@ namespace dslist {
 
 template<typename T>
 struct ListNode {
-    T data_;
-    ListNode<T>* next_;
+    using value_type = T;
+    using _self = ListNode<value_type>;
+
+    ListNode(const value_type& value, _self* prev, _self* next)
+    : value_(value)
+    , prev_(prev)
+    , next_(next) {
+    }
+
+    value_type value_;
+    _self* prev_;
+    _self* next_;
+};
+
+template<typename T>
+class List {
+    public:
+    using value_type = T;
+    using NodeType = ListNode<value_type>;
+    using size_type = std::size_t;
+
+    public:
+    List()
+    : node_(nullptr)
+    , size_(0) {
+    }
+
+    void push_back(const value_type& value) {
+        NodeType* node = new NodeType(value, node_, nullptr);
+        if (node_ == nullptr) {
+            node_ = node;
+        } else {
+            node_->next_ = node;
+        }
+        ++size_;
+    }
+
+    size_type size() const {
+        return size_;
+    }
+
+    private:
+    NodeType* node_;
+    size_type size_;
 };
 
 }
