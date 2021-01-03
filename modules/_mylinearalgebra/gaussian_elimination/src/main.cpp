bb3d55526cfcc963275ff9688d7c4db05061d0fa
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2021-01-03 22:20:05 +0200

    cosmetic changes

--- modules/_mylinearalgebra/gaussian_elimination/src/main.cpp
+++ modules/_mylinearalgebra/gaussian_elimination/src/main.cpp
@@ -5,9 +5,9 @@
 #include <chrono>
 
 // http://www.cleverstudents.ru/systems/solving_systems_Gauss_method.html
-// https://prog-cpp.ru/gauss/
 
-void dump(const std::vector<std::vector<double>>& x, const std::vector<double>& y) {
+void dump(const std::vector<std::vector<double>>& x,
+          const std::vector<double>& y) {
     const int setw_size = 2;
     for (size_t i = 0; i < x.size(); ++i) {
         for (size_t j = 0; j < x.size(); ++j) {
@@ -17,7 +17,9 @@ void dump(const std::vector<std::vector<double>>& x, const std::vector<double>& 
     }
 }
 
-void generate_matrix_01(const size_t size, std::vector<std::vector<double>>& x, std::vector<double>& y) {
+void generate_matrix_01(const size_t size,
+                        std::vector<std::vector<double>>& x,
+                        std::vector<double>& y) {
     for (size_t i = 0; i < size; ++i) {
         y.at(i) = static_cast<int>(i) + 1;
         for (size_t j = 0; j < size; ++j) {
@@ -31,9 +33,50 @@ void generate_matrix_01(const size_t size, std::vector<std::vector<double>>& x, 
     }
 }
 
+void solve_gauss(const size_t& size, std::vector<std::vector<double>>& x,
+                std::vector<double>& y,
+                std::map<int, double>& x_res) {
+    for (size_t i = 0; i < x.size() - 1; ++i) {
+        for (size_t j = i + 1; j < x.size(); ++j) {
+            double a = -x[j][i] / x[i][i];
+            if (size <= 10) {
+                std::cout << "a=" << a << std::endl;
+            }
+
+            double tmp_y = a * y[i];
+            y[j] = y[j] + tmp_y;
+
+            std::vector<double> tmp_x;
+            for (size_t t = 0; t < x[i].size(); ++t) {
+                tmp_x.push_back(x[i][t] * a);
+            }
+
+            for (size_t t = 0; t < tmp_x.size(); ++t) {
+                x[j][t] = x[j][t] + tmp_x[t];
+            }
+        }
+        if (size <= 10) {
+            dump(x, y);
+        }
+    }
+
+    for (int i = x.size() - 1; i >= 0; --i) {
+        double xi_res = y[i];
+        for (int j = x[i].size() - 1; j >= 0; --j) {
+            if (x[i][j] != 0 && x_res.count(j) == 0) {
+                x_res[j] = xi_res / x[i][j];
+                break;
+            }
+            else if (x_res.count(j) != 0) {
+                xi_res += -(x[i][j] * x_res.at(j));
+            }
+        }
+    }
+}
+
 int main(const int argc, const char* argv[]) {
     try {
-        std::vector<int> size_list({ 100,500,1000,2000,3000 });
+        std::vector<int> size_list({ 10,100,500,1000,2000,3000 });
         for (size_t size_index = 0; size_index < size_list.size(); ++size_index) {
             size_t size = size_list[size_index];
             std::vector<std::vector<double>> x(size, std::vector<double>(size, 0));
@@ -46,45 +89,12 @@ int main(const int argc, const char* argv[]) {
 
             std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-            for (size_t i = 0; i < x.size() - 1; ++i) {
-                for (size_t j = i + 1; j < x.size(); ++j) {
-                    double a = -x[j][i] / x[i][i];
-                    if (size <= 10) {
-                        std::cout << "a=" << a << std::endl;
-                    }
-
-                    double tmp_y = a * y[i];
-                    y[j] = y[j] + tmp_y;
-
-                    std::vector<double> tmp_x;
-                    for (size_t t = 0; t < x[i].size(); ++t) {
-                        tmp_x.push_back(x[i][t] * a);
-                    }
-
-                    for (size_t t = 0; t < tmp_x.size(); ++t) {
-                        x[j][t] = x[j][t] + tmp_x[t];
-                    }
-                }
-                if (size <= 10) {
-                    dump(x, y);
-                }
-            }
-
             std::map<int, double> x_res;
-            for (int i = x.size() - 1; i >= 0; --i) {
-                double xi_res = y[i];
-                for (int j = x[i].size() - 1; j >= 0; --j) {
-                    if (x[i][j] != 0 && x_res.count(j) == 0) {
-                        x_res[j] = xi_res / x[i][j];
-                        break;
-                    }
-                    else if (x_res.count(j) != 0) {
-                        xi_res += -(x[i][j] * x_res.at(j));
-                    }
-                }
-            }
+            solve_gauss(size, x, y, x_res);
+
             std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now();
-            std::cout << "Time difference milliseconds:" << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " size:" << size << std::endl;
+            std::cout << "Time difference milliseconds:" << std::chrono::duration_cast<
+                std::chrono::milliseconds>(stop - start).count() << " size:" << size << std::endl;
 
             if (size <= 10) {
                 std::map<int, double>::const_iterator it = x_res.begin();
