8003012b47c1295543d3200f7417ebeac2b5f9b3
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2021-01-03 14:13:15 +0200

    generated matrix and vector with 0 and 1

--- modules/_mylinearalgebra/gaussian_elimination/src/main.cpp
+++ modules/_mylinearalgebra/gaussian_elimination/src/main.cpp
@@ -7,7 +7,7 @@
 // https://prog-cpp.ru/gauss/
 
 void dump(const std::vector<std::vector<double>>& x, const std::vector<double>& y) {
-    const int setw_size = 10;
+    const int setw_size = 2;
     for (size_t i = 0; i < x.size(); ++i) {
         for (size_t j = 0; j < x.size(); ++j) {
             std::cout << std::setw(setw_size) << x[i][j] << " ";
@@ -16,61 +16,98 @@ void dump(const std::vector<std::vector<double>>& x, const std::vector<double>& 
     }
 }
 
+void generate_matrix_01(const size_t size, std::vector<std::vector<double>>& x, std::vector<double>& y) {
+    for (size_t i = 0; i < size; ++i) {
+        y.at(i) = i + 1;
+        for (size_t j = 0; j < size; ++j) {
+            if (j <= i) {
+                x.at(i).at(j) = 1;
+            }
+            else {
+                x.at(i).at(j) = 0;
+            }
+        }
+    }
+}
+
 int main(const int argc, const char* argv[]) {
-    std::vector<std::vector<double>> x = {
-        {3,2,1,1},
-        {1,-1,4,-1},
-        {-2,-2,-3,1},
-        {1,5,-1,2}
-//         {2,4,1},
-//         {5,2,1},
-//         {2,3,4},
-    };
-    std::vector<double> y = 
-        { -2,-1,9,4 }
-//        { 36, 47, 37 }
-    ;
+    try {
+//         std::vector<std::vector<double>> x = {
+//             {3,2,1,1},
+//             {1,-1,4,-1},
+//             {-2,-2,-3,1},
+//             {1,5,-1,2}
+// //             {2,4,1},
+// //             {5,2,1},
+// //             {2,3,4},
+//     };
 
-    dump(x, y);
+//     std::vector<double> y = 
+//         { -2,-1,9,4 }
+// //        { 36, 47, 37 }
+//     ;
 
-    for (size_t i = 0; i < x.size() - 1; ++i) {
-        for (size_t j = i+1; j < x.size(); ++j) {
-            double a = -x[j][i] / x[i][i];
-            std::cout << "a=" << a << std::endl;
+        size_t size = 0;
+        std::cout << "Enter size:";
+        std::cin >> size;
+        std::vector<std::vector<double>> x(size, std::vector<double>(size, 0));
+        std::vector<double> y(size, 0);
 
-            double tmp_y = a * y[i];
-            y[j] = y[j] + tmp_y;
+        generate_matrix_01(size, x, y);
+        if (size <= 10) {
+            dump(x, y);
+        }
 
-            std::vector<double> tmp_x;
-            for (size_t t = 0; t < x[i].size(); ++t) {
-                tmp_x.push_back(x[i][t] * a);
+        for (size_t i = 0; i < x.size() - 1; ++i) {
+            for (size_t j = i+1; j < x.size(); ++j) {
+                double a = -x[j][i] / x[i][i];
+                if (size <= 10) {
+                    std::cout << "a=" << a << std::endl;
+                }
+
+                double tmp_y = a * y[i];
+                y[j] = y[j] + tmp_y;
+
+                std::vector<double> tmp_x;
+                for (size_t t = 0; t < x[i].size(); ++t) {
+                    tmp_x.push_back(x[i][t] * a);
+                }
+
+                for (size_t t = 0; t < tmp_x.size(); ++t) {
+                    x[j][t] = x[j][t] + tmp_x[t];
+                }
             }
-
-            for (size_t t = 0; t < tmp_x.size(); ++t) {
-                x[j][t] = x[j][t] + tmp_x[t];
+            if (size <= 10) {
+                dump(x, y);
             }
         }
-        dump(x, y);
-    }
 
-    std::map<int, double> x_res;
-    for (int i = x.size() - 1; i >= 0; --i) {
-        double xi_res = y[i];
-        for (int j = x[i].size() - 1; j >= 0; --j) {
-            if (x[i][j] != 0 && x_res.count(j) == 0) {
-                x_res[j] = xi_res / x[i][j];
-                break;
-            }
-            else if (x_res.count(j) != 0) {
-                xi_res += -(x[i][j] * x_res.at(j));
+        std::map<int, double> x_res;
+        for (int i = x.size() - 1; i >= 0; --i) {
+            double xi_res = y[i];
+            for (int j = x[i].size() - 1; j >= 0; --j) {
+                if (x[i][j] != 0 && x_res.count(j) == 0) {
+                    x_res[j] = xi_res / x[i][j];
+                    break;
+                }
+                else if (x_res.count(j) != 0) {
+                    xi_res += -(x[i][j] * x_res.at(j));
+                }
             }
         }
-    }
 
-    std::map<int, double>::const_iterator it = x_res.begin();
-    for (it; it != x_res.end(); ++it) {
-        std::cout << "x"<<it->first + 1 << "=" << it->second << " ";
+        if (size <= 10) {
+            std::map<int, double>::const_iterator it = x_res.begin();
+            for (it; it != x_res.end(); ++it) {
+                std::cout << "x" << it->first + 1 << "=" << it->second << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "Done!" << std::endl;
     }
-
+    catch (const std::exception& e) {
+        std::cerr << "Error occurred:" << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
